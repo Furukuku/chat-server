85bf9fa6c446fbed3b2245d478017c6fc54170be
Furukuku <perezkmlagmay@gmail.com>
2024-09-14 12:37:45 +0800

    Added middleware for checking if the user is authenticated.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import "dotenv/config";
+import jwt from "jsonwebtoken";
 import { MessageController as Message } from "./controllers/MessageController";
 import { ConversationController as Conversation } from "./controllers/ConversationController";
 
@@ -15,13 +16,34 @@ const io = new Server(httpServer, {
   }
 });
 
+io.use((socket, next) => {
+  const authorization = socket.handshake.headers.authorization;
+  
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new Error('Invalid Token!'));
+  }
+  
+  const token = authorization.substring(7);
+  const accessKey = process.env.JWT_KEY || 'my-key';
+  
+  jwt.verify(token, accessKey, (err, decoded) => {
+    if (err) {
+      return next(new Error('Invalid Token!'));
+    }
+    
+    socket.data.user = decoded;
+    next();
+  });
+  
+});
+
 io.on("connection", (socket) => {
   socket.on('setRoom', (data) => {
     socket.join(data.conversationId);
   });
 
   socket.on('sendMessage', async (data, callback) => {
-    const newMessage = await Message.create(data.senderId, data.conversationId, data.content);
+    const newMessage = await Message.create(socket.data.user.user_id, data.conversationId, data.content);
     const status = { ok: false };
 
     if (newMessage) {
@@ -34,14 +56,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on('sendFirstMessage', async (data, callback) => {
-    const conversation = await Conversation.create(data.userId, data.clientId);
+    const conversation = await Conversation.create(socket.data.user.user_id, data.clientId);
     const response: { ok: boolean; conversationId: number | null } = { 
       ok: false ,
       conversationId: null
     };
 
     if (conversation) {
-      const newMessage = await Message.create(data.userId, conversation.id, data.content);
+      const newMessage = await Message.create(socket.data.user.user_id, conversation.id, data.content);
       
       if (newMessage) {
         response.ok = true;
